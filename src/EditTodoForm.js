05ec05d71e3e39d4f9ecfdbea4086e525960bb01
Mudroch.FE s.r.o. <wanrltv3328@gmail.com>
2024-10-17 17:43:10 +0200

    learning

--- src/EditTodoForm.js
+++ src/EditTodoForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {TextField} from "@mui/material";
 import useInputState from '../src/hooks/useInputState'
 import Button from '@mui/material/Button';
@@ -6,21 +6,34 @@ import Button from '@mui/material/Button';
 
 function EditTodoForm({editTodo, id, task, toggleEditForm}) {
     const [value, handleChange, reset] = useInputState(task)
-
+    const [error, setError] = useState(false);
+    const handleError = () => {
+        if (value.trim() === "" || value.length === 0) {
+            setError(true);
+            return true;
+        } else {
+            setError(false);
+            return false;
+        }
+    };
     return (
         <form
             onSubmit={(e) => {
-            e.preventDefault()
-            editTodo(id, value)
-            reset();
-            toggleEditForm()
-        }} style={{ width: "100%"}}>
+                e.preventDefault()
+                if(handleError())  return;
+                editTodo(id, value);
+                toggleEditForm();
+                reset();
+
+            }} style={{width: "100%"}}>
             <TextField
                 value={value}
                 onChange={handleChange}
                 fullWidth
                 autoFocus
                 style={{marginBottom: "1rem"}}
+                error={error}
+                helperText={error && "Input is empty"}
             />
             <Button fullWidth variant="contained" type="submit" color="primary">Update Todo</Button>
         </form>
--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, {useState} from "react";
 import Paper from "@mui/material/Paper";
 import {TextField} from "@mui/material";
 import useInputState from "./hooks/useInputState"
 import Button from "@mui/material/Button";
 
+
 function TodoForm({addTodo}) {
     const [value, handleChange, reset] = useInputState("");
+    const [error, setError] = useState(false);
+    const handleError = () => {
+        if (value === "" || value.length === 0) {
+            setError(true);
+            return true;
+        } else {
+            setError(false);
+            return false;
+        }
+    }
+
     return (
-        <Paper style={{margin: "1rem 0", padding: "1rem"}}>
-            <form onSubmit={(event) => {
-                event.preventDefault()
-                addTodo(value)
-                reset();
-            }}>
-                <TextField
-                    style={{marginBottom: "1rem", marginTop: "0"}}
-                    value={value}
-                    onChange={handleChange}
-                    margin="normal"
-                    label="Add new Todo"
-                    fullWidth
-                />
-                <Button fullWidth variant="contained" type="submit" color="primary">add todo</Button>
-            </form>
-        </Paper>
+        <>
+            <Paper style={{margin: "1rem 0", padding: "1rem"}}>
+                <form onSubmit={(event) => {
+                    event.preventDefault();
+                    if (handleError()) return;
+                    addTodo(value);
+                    reset();
+                }}>
+                    <TextField
+                        style={{marginBottom: "1rem", marginTop: "0"}}
+                        value={value}
+                        onChange={handleChange}
+                        margin="normal"
+                        label="Add new Todo"
+                        error={error}
+                        helperText={error && "Input is empty"}
+                        fullWidth
+                    />
+                    <Button fullWidth variant="contained" type="submit" color="primary">add todo</Button>
+                </form>
+
+            </Paper>
+        </>
     )
 }
 
